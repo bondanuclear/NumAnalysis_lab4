769ba7825938908d054a6af68ffd0efbb711b8e3
Andrii Bondarenko <lanbordik13@gmail.com>
2021-11-22 01:02:15 +0200

    Finished 4th lab

--- NA_lab4/src/com/polynom.java
+++ NA_lab4/src/com/polynom.java
@@ -20,5 +20,19 @@ public class polynom {
         for (int i = 0; i < storeArray.size(); i++) {
             System.out.println("y("+i+") = " + storeArray.get(i));
         }
+        System.out.println("===================================================");
+        for(BigInteger i = BigInteger.ONE; i.compareTo(BigInteger.valueOf(14)) < 0; i =i.add(BigInteger.ONE)) {
+            for(BigInteger j = BigInteger.ONE; j.compareTo(BigInteger.valueOf(14)) < 0; j =j.add(BigInteger.ONE)) {
+                System.out.println(i + "^" + j + " = " + i.pow(j.intValue()));
+            }
+        }
+        System.out.println("1 0 0 0 0 0 0 0 0 0 0 0 0 0  0");
+        for(BigInteger i = BigInteger.ONE; i.compareTo(BigInteger.valueOf(14)) < 0; i =i.add(BigInteger.ONE)) {
+            System.out.print("1 ");
+            for(BigInteger j = BigInteger.ONE; j.compareTo(BigInteger.valueOf(14)) < 0; j =j.add(BigInteger.ONE)) {
+                System.out.print(i.pow(j.intValue()) + " " );
+            }
+            System.out.println(" " + storeArray.get(i.intValue()));
+        }
     }
 }
